6ee1435e9674f386a9cd42e7d4fa25e28363b8d5
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-07 03:05:32 +0600

    userinput

---
+++ userinput.py
@@ -0,0 +1,9 @@
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+full_name = first_name + " " + last_name
+print(f"Your full name is {full_name}")

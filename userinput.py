8d6199740abaa51dc58d7324dfd868fede849df4
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-07 03:15:11 +0600

    input

--- userinput.py
+++ userinput.py
@@ -3,7 +3,10 @@ last_name = "Doe"
 full_name = first_name + " " + last_name
 print(full_name)
 
-first_name = input("What is your first name? ")
-last_name = input("What is your last name? ")
-full_name = first_name + " " + last_name
-print(f"Your full name is {full_name}")
+
+gift1= input("What is your first gift? ")
+gift2= input("What is your second gift? ")
+gift3= input("What is your third gift? ")
+
+print("For my Birthday i want a " + gift1 + ", a " + gift2 + " and a " + gift3 + ".")
+

8b12b4d356e2d5a05842fe183049580526dd3241
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-08 03:13:18 +0600

    first function

---
+++ function.py
@@ -0,0 +1,7 @@
+
+
+def calculateArea(length, width):
+    area = length * width
+    print("Area is", area)  # Print inside the function
+
+calculateArea(10, 20)
--- userinput.py
+++ userinput.py
@@ -10,3 +10,4 @@ gift3= input("What is your third gift? ")
 
 print("For my Birthday i want a " + gift1 + ", a " + gift2 + " and a " + gift3 + ".")
 
+

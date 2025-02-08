1febf6b3b70e90b03a7c1e2396a3e50634561604
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-09 02:19:38 +0600

    inheritance

--- OOP/class.py
+++ OOP/class.py
@@ -1,11 +1,22 @@
-class Dog:  # ক্লাস
-    def __init__(self, name, age):  # কনস্ট্রাক্টর
-        self.name = name  # attribute (ডাটা)
-        self.age = age    # attribute (ডাটা)
+# class Dog:  # ক্লাস
+#     def __init__(self, name, age):  # কনস্ট্রাক্টর
+#         self.name = name  # attribute (ডাটা)
+#         self.age = age    # attribute (ডাটা)
 
-    def bark(self):  # method (বিহেভিয়ার)
-        print(f"{self.name} is barking!")
+#     def bark(self):  # method (বিহেভিয়ার)
+#         print(f"{self.name} is barking!")
 
-# অবজেক্ট তৈরি
-my_dog = Dog("Tommy", 3)
-my_dog.bark()  # Output: Tommy is barking!
+# # অবজেক্ট তৈরি
+# my_dog = Dog("Tommy", 3)
+# my_dog.bark()  # Output: Tommy is barking!
+
+class Student:
+    def __init__(self, roll, gpa):
+        self.roll = roll
+        self.gpa = gpa
+
+    def display(self):
+        print(f"Roll: {self.roll}, GPA: {self.gpa}")
+
+sabilar = Student(3609, 3.36)
+sabilar.display()

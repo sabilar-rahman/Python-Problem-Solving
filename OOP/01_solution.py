4279f159a5542ac0526ec41a4d9621733afca295
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-09 01:22:06 +0600

    basic class and object

--- OOP/01_solution.py
+++ OOP/01_solution.py
@@ -1,5 +1,10 @@
 class Car:
-    brand=None
-    model=None
-my_Car= Car()
-print(my_Car)
+    def __init__(self,userBrand, userModel):
+        self.brand = userBrand
+        self.model = userModel
+
+my_Car= Car("Toyota", "Camry")
+print(my_Car.brand)
+
+my_new_car = Car("Honda", "Civic")
+print(my_new_car.model)

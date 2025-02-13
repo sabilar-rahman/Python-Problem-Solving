0a324da7407406241aa3633e03e99aef51f353fd
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-14 02:40:05 +0600

    Adding Elements into List

---
+++ DSA/array/Adding Elements into List.py
@@ -0,0 +1,14 @@
+# Initialize an empty list
+a = []
+
+# Adding 10 to end of list
+a.append(10)  
+print("After append(10):", a)  
+
+# Inserting 5 at index 0
+a.insert(0, 5)
+print("After insert(0, 5):", a) 
+
+# Adding multiple elements  [15, 20, 25] at the end
+a.extend([15, 20, 25])  
+print("After extend([15, 20, 25]):", a) 
